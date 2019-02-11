6705269d25af71ac7fe7984a114061f305938f01
M <Michal.Plsek@seznam.cz>
2019-02-11 23:03:59 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -1,21 +1,44 @@
 #!/usr/bin/env python3
 
 import socket
-import codecs
 import re
 import platform
+import sys
+import subprocess
+
+def getcpu():
+    curr = re.compile("^CPU MHz:\s*[0-9]+\.[0-9]+$")
+    max = re.compile("^CPU max MHz:\s*[0-9]+\.[0-9]+$")
+    fl = re.compile("\d+.?\d+")
+    foundcurr = False
+    foundmax = False
+    test = subprocess.Popen(["lscpu"], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
+    output = test.communicate()[0]    
+    text = output.split('\n')
+    for t in text:
+        if re.match(max,t):
+            maxlist = [float(i) for i in re.findall(fl,t)]
+            foundmax = True
+        elif re.match(curr,t):
+            currlist = [float(i) for i in re.findall(fl,t)]
+            foundcurr = True
+    if foundmax and foundcurr:
+        return str(int(currlist[0]/maxlist[0]*100)) + '%'
+    else:
+        print("Chyba pri lscpu.")
+        sys.exit(1)
 
 # re typ pozadavku
-hostname = re.compile("^GET\s+\/hostname")
-cpuname = re.compile("^GET\s+\/cpu-name")
-load = re.compile("^GET\s+\/load\s+")
-loadr = re.compile("^GET\s+\/load\?refresh=[0-9]+\s+")
+hostname = re.compile("^GET\s+\/hostname.*$")
+cpuname = re.compile("^GET\s+\/cpu-name.*$")
+load = re.compile("^GET\s+\/load\s+.*$")
+loadr = re.compile("^GET\s+\/load\?refresh=[0-9]+\s+.*$")
 # re typ accept
-tp = re.compile("^Accept:\s+text\/plain$")
-aj = re.compile("^Accept:\s+application\/json$")
+tp = re.compile("^Accept:\s+text\/plain$.*$")
+aj = re.compile("^Accept:\s+application\/json$.*$")
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(("merlin.fit.vutbr.cz",12345))
+s.bind(("merlin.fit.vutbr.cz",12225))
 s.listen(1)
 conn,address = s.accept() # conn = socket na druhe strane, address = tuple ve formatu addr + port druhe strany
 
@@ -25,6 +48,7 @@ while 1:
         break
     text = data.decode().split('\r\n')
     
+    ToJson = False # budeme prevadet na JSON
     FoundType = False # zatim nenalezl na zadnem radku typ requestu
     FoundAccept = False # zatim nenalezl na zadnem radku Accept type    
     for line in text:
@@ -32,131 +56,32 @@ while 1:
         if not FoundType:
             if re.match(line,hostname):
                 FoundType = True
+                typ = "hostname"
                 data = socket.gethostname()
             elif re.match(line,cpuname):
                 FoundType = True
+                typ = "cpu"
                 data = platform.processor()
             elif re.match(line,load):
                 FoundType = True
-                data = "25%"
+                typ = "zatizeni"
+                data = getcpu()
             elif re.match(line,loadr):
                 FoundType = True
-                data="25%r"
+                typ = "zatizeni"
+                data = getcpu()
         if not FoundAccept:
             if re.match(line,aj):
                 FoundAccept = True
-                print("Vygeneruj Accept format A/J") # ciste jako string, do json to zpracuje TCP
+                ToJson = True
             elif re.match(line,tp):
                 FoundAccept = True
-                print("Vygeneruj Accept format T/P")
-                
-    if FoundType and not FoundAccept: # typ nalezen, ale nebyl zadan Accept typ, poslat defaultne text/plain a odeslat
-        print("Vygeneruj Accept t/p")        
-    elif not FoundType: # nebyl nalezen typ, exception
-        print("Exception")
-    else: # nalezeny oba, odeslat
-        
-    # odesilani    
-    conn.sendall(data)
-    
-    @author: plagtag
-'''
-from time import sleep
-import sys
-
-class GetCpuLoad(object):
-    '''
-    classdocs
-    '''
-
-
-    def __init__(self, percentage=True, sleeptime = 1):
-        '''
-        @parent class: GetCpuLoad
-        @date: 04.12.2014
-        @author: plagtag
-        @info: 
-        @param:
-        @return: CPU load in percentage
-        '''
-        self.percentage = percentage
-        self.cpustat = '/proc/stat'
-        self.sep = ' ' 
-        self.sleeptime = sleeptime
-
-    def getcputime(self):
-        '''
-        http://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
-        read in cpu information from file
-        The meanings of the columns are as follows, from left to right:
-            0cpuid: number of cpu
-            1user: normal processes executing in user mode
-            2nice: niced processes executing in user mode
-            3system: processes executing in kernel mode
-            4idle: twiddling thumbs
-            5iowait: waiting for I/O to complete
-            6irq: servicing interrupts
-            7softirq: servicing softirqs
-
-        #the formulas from htop 
-             user    nice   system  idle      iowait irq   softirq  steal  guest  guest_nice
-        cpu  74608   2520   24433   1117073   6176   4054  0        0      0      0
-
-
-        Idle=idle+iowait
-        NonIdle=user+nice+system+irq+softirq+steal
-        Total=Idle+NonIdle # first line of file for all cpus
-
-        CPU_Percentage=((Total-PrevTotal)-(Idle-PrevIdle))/(Total-PrevTotal)
-        '''
-        cpu_infos = {} #collect here the information
-        with open(self.cpustat,'r') as f_stat:
-            lines = [line.split(self.sep) for content in f_stat.readlines() for line in content.split('\n') if line.startswith('cpu')]
-
-            #compute for every cpu
-            for cpu_line in lines:
-                if '' in cpu_line: cpu_line.remove('')#remove empty elements
-                cpu_line = [cpu_line[0]]+[float(i) for i in cpu_line[1:]]#type casting
-                cpu_id,user,nice,system,idle,iowait,irq,softrig,steal,guest,guest_nice = cpu_line
-
-                Idle=idle+iowait
-                NonIdle=user+nice+system+irq+softrig+steal
-
-                Total=Idle+NonIdle
-                #update dictionionary
-                cpu_infos.update({cpu_id:{'total':Total,'idle':Idle}})
-            return cpu_infos
-
-    def getcpuload(self):
-        '''
-        CPU_Percentage=((Total-PrevTotal)-(Idle-PrevIdle))/(Total-PrevTotal)
-
-        '''
-        start = self.getcputime()
-        #wait a second
-        sleep(self.sleeptime)
-        stop = self.getcputime()
-
-        cpu_load = {}
-
-        for cpu in start:
-            Total = stop[cpu]['total']
-            PrevTotal = start[cpu]['total']
-
-            Idle = stop[cpu]['idle']
-            PrevIdle = start[cpu]['idle']
-            CPU_Percentage=((Total-PrevTotal)-(Idle-PrevIdle))/(Total-PrevTotal)*100
-            cpu_load.update({cpu: CPU_Percentage})
-        return cpu_load
-
-
-if __name__=='__main__':
-    x = GetCpuLoad()
-    while True:
-        try:
-            data = x.getcpuload()
-            print data
-        except KeyboardInterrupt:
-
-            sys.exit("Finished")                
+      
+    if (FoundType and FoundAccept) or (FoundType and not FoundAccept): # nalezeny oba, rid se podle typu Accept || nalezen jen typ, Accept automaticky na text/plain
+        if ToJson: # preved na JSON, typ se nastavoval uz v analyze
+            data = "{ \"typ : " + typ + "\" , \"hodnota : " + data + "\" }"
+        conn.sendall(data)             
+    else: # nebyl nalezen typ, exception
+        print("Spatny typ requestu.")
+        sys.exit(1)
 
