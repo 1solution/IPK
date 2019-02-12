e753b195a9d5a9ef77d0708e393162adea9c9b0f
M <Michal.Plsek@seznam.cz>
2019-02-12 11:42:27 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -12,15 +12,6 @@ class CpuError(Exception): # vlastni vyjimka, chyba pri volani lscpu
 class RequestError(Exception): # vlastni vyjimka, request ma blbou strukturu
     pass
 
-def refr(): # vytvor text request=, ktery posles s.send(request.encode()) 
-    #request = "POST / HTTP/1.1\nHost: " + server + "\nPath: " + path + "\n\n"
-    #s.connect((socket.gethostbyname(server),port))
-    #s.send(request.encode())
-    # Co v nem musi byt:
-    #HTTP/1.1 200 OK
-    #Refresh: 5; url=http://www.example.org/fresh-as-a-summer-breeze 
-    pass
-
 def getcpu(): # vraci % zatizeni cpu
     curr = re.compile("^CPU MHz:\s*[0-9]+\.[0-9]+$")
     max = re.compile("^CPU max MHz:\s*[0-9]+\.[0-9]+$")
@@ -38,7 +29,7 @@ def getcpu(): # vraci % zatizeni cpu
             currlist = [float(i) for i in re.findall(fl,t)]
             foundcurr = True
     if foundmax and foundcurr:
-        return str(int(currlist[0]/maxlist[0]*100)) + '%'
+        return str(int(currlist[0]/maxlist[0]*100)) + '%\n'
     else:
         return ''
 
@@ -53,18 +44,21 @@ aj = re.compile("^Accept:\s+application\/json$.*$")
 # dec cislo, vyhledani refresh rate v radku
 dec = re.compile("\d+")
 
+arg_address = "merlin.fit.vutbr.cz" # argument z make
+arg_port = 12225 # argument z make
+
 try:
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.bind(("merlin.fit.vutbr.cz",12225))
+    s.bind((arg_address,arg_port))
     s.listen(1)
-    conn,address = s.accept() # conn = socket na druhe strane, address = tuple ve formatu addr + port druhe strany
+    client,address = s.accept() # conn = socket na druhe strane, address = tuple ve formatu addr + port druhe strany
     
     while 1:
-        data = conn.recv(1024)
+        data = client.recv(1024)
         if not data:
             break
         text = data.decode().split('\r\n')
-        
+
         CustomRequest = False
         ToJson = False # budeme prevadet na JSON
         FoundType = False # zatim nenalezl na zadnem radku typ requestu
@@ -73,17 +67,28 @@ try:
             # validace typu requestu GET /.....
             if not FoundType:
                 
-                if re.match(line,hostname):
+                if re.match(hostname,line): # vrat hostname
                     FoundType = True
                     typ = "hostname"
                     data = socket.gethostname()
                     
-                elif re.match(line,cpuname):
+                elif re.match(cpuname,line): # vrat nazev cpu
                     FoundType = True
                     typ = "cpu"
-                    data = platform.processor()
-                    
-                elif re.match(line,loadr):
+                    cpuinfo = subprocess.Popen(["cat /proc/cpuinfo"], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
+                    output = cpuinfo.communicate()[0]
+                    text = output.split('\n')
+                    model = re.compile("^model name.*$")
+                    model_name = re.compile("(?<=: ).*$")
+                    for t in text:
+                        print(t)
+                        if re.match(model,t):
+                            print('NALEZENO.')
+                            data = re.findall(model_name,t)
+                            data = data[0]
+                            break
+
+                elif re.match(loadr,line): # vrat zatez a do Accept pridej refresh
                     FoundType = True
                     typ = "zatizeni"
                     data = getcpu()
@@ -95,7 +100,7 @@ try:
                     refresh = refresh[0] # vyber refresh rate
                     CustomRequest = True # budes vytvaret vlastni textovy request k odeslani
                     
-                elif re.match(line,load):
+                elif re.match(load,line): # vrat zatez
                     FoundType = True
                     typ = "zatizeni"
                     data = getcpu()
@@ -103,20 +108,30 @@ try:
                         raise CpuError
                         
             if not FoundAccept:
-                if re.match(line,aj):
+                if re.match(aj,line):
                     FoundAccept = True
                     ToJson = True
-                elif re.match(line,tp):
+                elif re.match(tp,line):
                     FoundAccept = True
           
         if (FoundType and FoundAccept) or (FoundType and not FoundAccept): # nalezeny oba, rid se podle typu Accept || nalezen jen typ, Accept automaticky na text/plain          
             if ToJson: # preved na JSON, typ se nastavoval uz v analyze
                 data = "{ \"typ : " + typ + "\" , \"hodnota : " + data + "\" }"
-            if CustomRequest: # vytvor kvuli refreshi vlastni textovy request a ten odesli
-                data = refr()
-            conn.sendall(data) # odeslani requestu         
+                content_type = "application/json"
+            else:
+                content_type = "text/plain"
+
+            if CustomRequest: # vytvor POST request s refresh
+                refr_string = "Refresh: " + refresh + "; " + arg_address + ":" + str(arg_port) + "/load?refresh=" + refresh + "\n"
+            else: # vytvor obycejny POST request
+                refr_string = ''
+            #outcoming = "POST / HTTP/1.1\n" + refr_string + "Host: " + arg_address + ":" + str(arg_port) + " \nContent-Type: " + content_type + "\nContent-Length: " + str(len(data)) + "\n\n" + data
+            outcoming = data
+            client.sendall(outcoming.encode()) # odeslani requestu
         else: # nebyl nalezen typ, exception
             print("Nebyl nalezen typ requestu GET.")
+        client.close()
+    s.close()
 
 except socket.error:
     print("Starsi obecna chyba.")
